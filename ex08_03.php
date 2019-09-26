42e16a25908c3655835ab0b53a8bc4382efb5c4c
hitourebito <yuto0423777@gmail.com>
2019-09-26 16:05:49 +0900

    Check POST

--- ex08_03.php
+++ ex08_03.php
@@ -1,6 +1,14 @@
+<?php 
 $errmsg = array();
 $str    = "";
 $ichi   = "";
 $keta   = "";
 $msg    = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+  $str  = $_POST["str"];
+  $ichi = $_POST["ichi"];
+  $keta = $_POST["keta"];
+}
+?>
