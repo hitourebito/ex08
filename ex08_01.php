00153cefa5949dc858d476664bf0fd02764cb7ae
hitourebito <yuto0423777@gmail.com>
2019-09-26 14:13:33 +0900

    Check $su

--- ex08_01.php
+++ ex08_01.php
@@ -3,6 +3,12 @@ $errmsg = array();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
+  $su = $_POST["su"];
+
+  if (!strlen($su) || !is_numeric($su) || strpos($su, ".") !== FALSE) 
+  {
+    $errmsg[] = "整数が入力されていません";
+  }
 }
 
 ?>
