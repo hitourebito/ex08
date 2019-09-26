1e4e5024e9706e4e6c2d54bd32bef39ad182e0cd
hitourebito <yuto0423777@gmail.com>
2019-09-26 16:10:34 +0900

    Copy html base to ex08_03.php

--- ex08_03.php
+++ ex08_03.php
@@ -10,5 +10,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
   $str  = $_POST["str"];
   $ichi = $_POST["ichi"];
   $keta = $_POST["keta"];
+
+
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ex08_03.php</title>
+  <style>
+    #err 
+    {
+      color: red;
+    };
+  </style>
+</head>
+<body>
+  <h1>文字列抜き出し</h1>
+  <div id="err">
+    <?php 
+      foreach ($errmsg as $value) 
+      {
+        echo $value, "<br/>";
+      }
+    ?>
+  </div>
+  
+</body>
